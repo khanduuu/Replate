cc84ac515f0c83923b9bbcc9e2e3ecb92b26405d
khanduuu <prokhandu@gmail.com>
2026-06-28 18:29:35 +0530

    Prepare frontend for deployment

--- frontend/src/api/myDonation.ts
+++ frontend/src/api/myDonation.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:5000/api";
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:5000/api";
 
 export async function getMyDonations(token: string) {
   const response = await fetch(`${BASE_URL}/donation/my`, {
